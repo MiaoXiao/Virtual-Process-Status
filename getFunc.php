3296a410e3c76d0bb7c1a93f62b05688b4b8a21d
ealca005 <ealca005@users.noreply.github.com>
2016-05-31 05:16:24 -0700

    Add files via upload
    getFunc() - takes in $column, and $user. If no $user specified, enter 'nouser'

---
+++ getFunc.php
@@ -0,0 +1,47 @@
+<?php
+// input: query
+// output: string of numbers
+// function: always get pid, control what column you want
+// after: echo both strings
+
+// parameters: column, user
+// if no user, enter $userquery = 'nouser';
+function getFunc($column, $userquery)
+{
+	// sets up database, emits error and stops script if no database
+	require_once('../mysqli_connect.php');
+
+	// enter your own info here!!!
+	$con=mysqli_connect("localhost","root","4utoHeart","cs183");
+
+	$table = 'info';
+	$output = "";
+	$query = "";
+
+	if ($userquery == 'nouser')
+	{
+		$query = "SELECT * FROM $table
+				  ORDER BY timestamp DESC";
+	}
+	else 
+	{
+		$query = "SELECT * FROM $table
+			 WHERE user='$userquery'
+			 ORDER BY timestamp DESC";
+
+	}
+	
+
+	// run the query
+	$result = mysqli_query($con, $query);
+
+	// concatenate result
+	while ($row = $result->fetch_assoc()) {
+        $output = $output . $row[$column] . " ";
+        //echo $row['pid']."<br>";
+    }
+
+	echo $output;
+}
+
+?>
