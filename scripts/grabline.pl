cfec8361567cd269d91c1b57704eb9fcbb42fdf1
natashamorando <morando.natasha@gmail.com>
2016-05-31 10:35:08 -0700

    Add files via upload

--- scripts/grabline.pl
+++ scripts/grabline.pl
@@ -8,10 +8,19 @@ if($line eq "")
 {
   foreach $var(@lines)
   {
-    print "$var\n";
+    @notime=split(" ", $var);
+    if(!($notime[2] eq "0.0"))
+    {
+      print "$var\n";
+    }
   }
 }
 else
 {
-  print "$lines[$line]\n";
+  #print "$lines[$line]\n";
+  @notime=split(" ", $lines[$line]);
+  if(!($notime[2] eq "0.0"))
+  {
+     print $lines[$line];
+  }
 }
