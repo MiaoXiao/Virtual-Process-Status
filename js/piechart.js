22b51f33d1585f9bfe9f4b5d98cbefc6e4d4f661
Rica Feng <topgb@msn.com>
2016-06-02 03:05:13 -0700

    code organizatioon

---
+++ js/piechart.js
@@ -0,0 +1,133 @@
+//Load charts
+function loadCharts()
+{
+	google.charts.load('current', {'packages':['corechart']});
+}
+
+//Synchronously grabs chart data from php file
+function getChartData(cata, user)
+{
+	xmlhttp.open("GET", "php/getFunc.php?column=" + cata, false);
+	xmlhttp.send(null);
+
+	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		return xmlhttp.response;
+		//console.log(user);
+	}
+}
+
+//Display selected Pie Chart
+function displayPieChart(cata, user)
+{
+	document.getElementById("title").innerHTML = cata;
+	var cataArray = document.getElementsByTagName("LI");
+	//console.log(cataArray);
+	for (var i=0, max=cataArray.length; i < max; i++)
+	{
+		cataArray[i].setAttribute("class", "");
+	}
+
+	document.getElementById(cata).setAttribute("class", "active");
+	google.charts.setOnLoadCallback(drawChart);
+	function drawChart() 
+	{
+		// convert string to array
+		function toArray(arr)
+		{
+			arr = arr.split(" ");
+			var stringArr = new Array();
+
+			for (var i = 0; i < arr.length; i++) {
+				stringArr.push(arr[i]);
+			}
+			return stringArr;
+		}
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'pid');
+		data.addColumn('number', cata);
+		
+		var pid = getChartData(cata, user);
+		var category = getChartData(cata, user);
+		var command = getChartData(cata, user);
+		var user = getChartData(cata, user);
+		
+		/*
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "getFunc.php?column=pid", false);
+		xmlhttp.send(null);
+		
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				//console.log(xmlhttp.responseText);
+				pid = (xmlhttp.responseText);
+				console.log(pid);
+			}
+		
+		var catagory;
+		xmlhttp.open("GET", "getFunc.php?column=" + cata, false);
+		xmlhttp.send(null);
+		
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				//console.log(xmlhttp.responseText);
+				catagory = xmlhttp.response;
+				console.log(catagory);
+			}
+
+		var command;
+		xmlhttp.open("GET", "getFunc.php?column=command", false);
+		xmlhttp.send(null);
+		
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				//console.log(xmlhttp.responseText);
+				command = xmlhttp.response;
+				console.log(command);
+			}
+
+		var user;
+		xmlhttp.open("GET", "getFunc.php?column=user", false);
+		xmlhttp.send(null);
+		
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				//console.log(xmlhttp.responseText);
+				user = xmlhttp.response;
+				console.log(user);
+			}
+		*/
+		
+		/*
+		//TEST DATA
+		var pid = "1 2 3 4";
+		var catagory = "0.2 0.4 0.2 0.1";
+		var command = "chrome darksouls geany spotify";
+		var user = "rica alyza eliza jon";
+		*/
+		
+		var pidArr = toArray(pid);
+		var cataArray = toArray(catagory);
+		var commandAray = toArray(command);
+		var userArray = toArray(user);
+
+		var arg = [];
+		for(var i = 0; i < pidArr.length; i++)
+		{
+			arg.push(["PID: " + pidArr[i] + " " + commandAray[i] + " " + userArray[i], parseFloat(cataArray[i])]);
+		}
+		data.addRows(arg);
+
+		//Options for selected pie chart
+		var options = 
+		{
+			//None
+		};
+
+		var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+		chart.draw(data, options);
+	}
+}
+
+//On Page load function
+function onLoad(cata,user)
+{
+    loadCharts();
+    displayPieChart(cata,user);
+}
+
--- php/getFunc.php
+++ php/getFunc.php
@@ -1,14 +1,9 @@
 <?php
-// input: query
-// output: string of numbers
-// function: always get pid, control what column you want
-// after: echo both strings
+
+//column to pull from
 $column=$_GET["column"];
-$userquery="nouser";
-//echo $column;
-//echo $userquery;
-// parameters: column, user
 // if no user, enter $userquery = 'nouser';
+$userquery="nouser";
 
 // sets up database, emits error and stops script if no database
 require_once('../mysqli_connect.php');
