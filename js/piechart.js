8d8bfbacd5ac6c9d3f5045476acdb20b24dbea2a
alyzacm <amalu001@ucr.edu>
2016-06-03 15:54:45 -0700

    working on line graph and mysql

--- js/piechart.js
+++ js/piechart.js
@@ -282,6 +282,107 @@ function displayPieChart(cata, user)
 		var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 		chart.draw(data, options);
 	}
+}//Display selected line Chart
+function displaylineChart(cata, user)
+{
+	document.getElementById("title").innerHTML = cata;
+	var cataArray = document.getElementsByTagName("LI");
+	//console.log(cataArray);
+	for (var i=0, max=cataArray.length; i < max; i++)
+	{
+		cataArray[i].setAttribute("class", "");
+	}
+
+	document.getElementById(cata).setAttribute("class", "active");
+	google.charts.setOnLoadCallback(drawChart);
+	function drawChart() 
+	{
+		var data = new google.visualization.DataTable();
+		data.addColumn('number', 'time');
+		data.addColumn('number', cata);
+		
+		
+		//var pid = getChartData(cata, user);
+		//var category = getChartData(cata, user);
+		//var command = getChartData(cata, user);
+		//var user = getChartData(cata, user);
+		
+		
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "php/getFunc.php?column=pid", false);
+		xmlhttp.send(null);
+		
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				//console.log(xmlhttp.responseText);
+				pid = (xmlhttp.responseText);
+				console.log(pid);
+			}
+		
+		var catagory;
+		xmlhttp.open("GET", "php/getFunc.php?column=" + cata, false);
+		xmlhttp.send(null);
+		
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				//console.log(xmlhttp.responseText);
+				catagory = xmlhttp.response;
+				console.log(catagory);
+			}
+
+		var command;
+		xmlhttp.open("GET", "php/getFunc.php?column=command", false);
+		xmlhttp.send(null);
+		
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				//console.log(xmlhttp.responseText);
+				command = xmlhttp.response;
+				console.log(command);
+			}
+
+		var user;
+		xmlhttp.open("GET", "php/getFunc.php?column=user", false);
+		xmlhttp.send(null);
+		
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				//console.log(xmlhttp.responseText);
+				user = xmlhttp.response;
+				console.log(user);
+			}
+		
+		
+		
+		//TEST DATA
+		//var pid = "1 2 3 4";
+		//var catagory = "0.2 0.4 0.2 0.1";
+		//var command = "chrome darksouls geany spotify";
+		//var user = "rica alyza eliza jon";
+		
+		var pidArr = stringToArray(pid);
+		var cataArray = stringToArray(catagory);
+		var commandAray = stringToArray(command);
+		var userArray = stringToArray(user);
+
+		var arg = [];
+		for(var i = 0; i < pidArr.length; i++)
+		{
+			arg.push(["PID: " + pidArr[i] + " " + commandAray[i] + " " + userArray[i], parseFloat(cataArray[i])]);
+		}
+		data.addRows(arg);
+
+		//Options for selected pie chart
+		var options = 
+		{
+			hAxis: {
+				title: 'Time'
+			},
+			vAxis: {
+				title: cata
+			}
+
+		};
+
+		var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+		chart.draw(data, options);
+	}
 }
 
 //On Page load function
--- php/mysqli_connect.php
+++ php/mysqli_connect.php
@@ -4,15 +4,15 @@
 	// It should be saved outside of the main web documents folder
 	// and imported when needed
 
-	DEFINE ('DB_USER', 'jondominguez');
-	DEFINE ('DB_PASSWORD', 'j0rdan23');
-	DEFINE ('DB_HOST', 'localhost');
+	DEFINE ('DB_USER', 'root');
+	DEFINE ('DB_PASSWORD', 'Oroi?sh2');
+	DEFINE ('DB_HOST', '127.0.0.1');
 	DEFINE ('DB_NAME', 'cs183');
 	 
 	// $dbc will contain a resource link to the database
 	// @ keeps the error from showing in the browser
 	 
 	$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-	OR die('Could not connect to MySQL: ' .
+	OR die('Could not connect to MySQL bruh: ' .
 	mysqli_connect_error());
 ?>
